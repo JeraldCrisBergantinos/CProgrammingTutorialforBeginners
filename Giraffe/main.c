42f986c1f8fcdc38dd2cc8737c1a247665507b7d
JeraldCrisBergantinos <44352027+JeraldCrisBergantinos@users.noreply.github.com>
2024-01-07 08:32:33 +0800

    Printf

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -3,10 +3,12 @@
 
 int main()
 {
-    int age = 40;
-    double gpa = 3.7;
-    char grade = 'A';
-    char phrase[] = "Giraffe Academy";
+    printf("My favorite %s is %d\n", "number", 500);
+    printf("My favorite %s is %f\n", "number", 500.33847832);
+    int favNum = 90;
+    char myChar = 'i';
+    printf("My favorite %c is %d\n", myChar, favNum);
+    printf("My favorite %c is %f\n", myChar, favNum);
 
     return 0;
 }
