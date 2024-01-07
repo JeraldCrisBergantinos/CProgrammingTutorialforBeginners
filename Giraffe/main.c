3949c457de7479b4479a06c96f8a4cfd29842529
JeraldCrisBergantinos <44352027+JeraldCrisBergantinos@users.noreply.github.com>
2024-01-07 08:21:21 +0800

    Variables

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -3,10 +3,18 @@
 
 int main()
 {
-    printf("   /|\n");
-    printf("  / |\n");
-    printf(" /  |\n");
-    printf("/___|\n");
+    //printf("There once was a man named George\n");
+    //printf("he was 70 years old.\n");
+    //printf("He really liked the name George\n");
+    //printf("but did not like being 70.\n");
+
+    char characterName[] = "Tom";
+    int characterAge = 67;
+    printf("There once was a man named %s\n", characterName);
+    printf("he was %d years old.\n", characterAge);
+    characterAge = 30;
+    printf("He really liked the name %s\n", characterName);
+    printf("but did not like being %d.\n", characterAge);
 
     return 0;
 }
